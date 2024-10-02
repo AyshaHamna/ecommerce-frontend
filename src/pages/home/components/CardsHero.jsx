0d7277b9c9b9cf2ac9be625444d6cf63479379b0
Aysha Hamna <ayshahamna57@gmail.com>
2024-10-02 19:55:31 +0530

    image url updated

--- src/pages/home/components/CardsHero.jsx
+++ src/pages/home/components/CardsHero.jsx
@@ -3,19 +3,17 @@ import React from "react";
 function CardsHero() {
   return (
     <section className="grid grid-cols-1 md:grid md:grid-cols-2 gap-5 my-5 md:mb-10 md:min-h-[60vh]">
-
       <div className="bg-gray-200 rounded-lg flex items-center h-full">
         <img
-          src="/public/assets/hero/macbook.png"
+          src="/assets/hero/macbook.png"
           className=" w-full p-10 sm:p-20 object-contain"
         />
       </div>
 
       <div className="grid gap-5 h-full">
-
         <div className=" bg-gray-200 rounded-lg h-full">
           <img
-            src="/public/assets/hero/iphone16.png"
+            src="/assets/hero/iphone16.png"
             className=" p-5 h-full w-full object-contain"
           />
         </div>
@@ -23,17 +21,16 @@ function CardsHero() {
         <div className=" w-full gap-3 flex flex-row justify-around h-full">
           <div className="flex-grow bg-gray-200 rounded-lg h-full">
             <img
-              src="/public/assets/hero/apple-watch.png"
+              src="/assets/hero/apple-watch.png"
               className=" md:p-5 h-full w-full object-contain"
             />
           </div>
           <div className="flex-grow bg-gray-200 rounded-lg h-full">
             <img
-              src="/public/assets/hero/airpods-max.png"
+              src="/assets/hero/airpods-max.png"
               className=" h-full w-full object-contain md:p-5"
             />
           </div>
-
         </div>
       </div>
     </section>
